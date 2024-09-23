2657cf4004377c06fb2509830b3e3b3f5597b212
ciringa <thierrird957468@gmail.com>
2024-09-23 10:20:11 -0300

    new features

---
+++ prisma/migrations/20240923130043_prod_changes/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Product" ADD CONSTRAINT "Product_pkey" PRIMARY KEY ("Id");
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,12 @@ import { PrismaService } from './lib/prisma.service';
 import { UserService } from './services/user.service';
 import { inMemoryUserRepositorie } from './repositories/inMemory/user.repositorie';
 import { UserController } from './controllers/user.controller';
+import { ProductService } from './services/product.service';
 
 
 @Module({
   imports: [],
   controllers: [UserController],
-  providers: [PrismaService, UserService,inMemoryUserRepositorie],
+  providers: [PrismaService, UserService,inMemoryUserRepositorie, ProductService],
 })
 export class AppModule {}
